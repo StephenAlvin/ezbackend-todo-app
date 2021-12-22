3caee8895041de814330f2dc114fcf0da491b860
Stephen Alvin <stephenalvin0912@gmail.com>
2021-12-22 13:54:30 +0800

    Changed baseURL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios'
 
-const BASEURL = 'http://localhost:8000'
+// const BASEURL = 'http://localhost:8000'
+const BASEURL = 'https://ezbackend-todo-app-backend.herokuapp.com/'
+
 
 function App() {
 
